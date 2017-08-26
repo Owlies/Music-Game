d242697c789c1cbaa35430b4e0f9f50a65191336
owlies.zihan@gmail.com <owlies.zihan@gmail.com>
2017-08-26 06:01:37 -0700

    load hints!

---
+++ Music Game/Assets/Editor/MusicEditor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class MusicEditor {
+
+
+    [MenuItem("Music/Create Hints")]
+	// Use this for initialization
+	static void Start ()
+    {
+        GameObject anchor = GameObject.FindGameObjectWithTag("Hint");
+        if(anchor)
+            GameObject.DestroyImmediate(anchor);
+        GameObject hint = new GameObject("Hints");
+        hint.tag = "Hint";
+
+        string path = EditorUtility.OpenFilePanel("Load onSet file", "", "");
+
+        StreamReader sr = File.OpenText(path);
+        string str;
+        while ((str = sr.ReadLine()) != null)
+        {
+            float time = float.Parse(str);
+            GameObject a = GameObject.Instantiate(Resources.Load("Prefabs/Hint")) as GameObject;
+            a.transform.parent = hint.transform;
+
+
+            MusicController musicCtrl = GameObject.FindGameObjectWithTag("MusicController").GetComponent<MusicController>();
+            PlayerController playerCtrl = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+
+            float offset = (musicCtrl.startDelay + 0.1f + time) * playerCtrl.horizontalSpeed;
+
+            a.transform.position = new Vector3(offset, 0, 0);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
